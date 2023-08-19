611aae5d6e1d3d0d5ea643ccf3504050a7dc94b7
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2023-08-19 12:49:39 +0900

    feat: Update network object interface

--- src/Crossoverse.Core/Domain/Multiplayer/INetworkObject.cs
+++ src/Crossoverse.Core/Domain/Multiplayer/INetworkObject.cs
@@ -5,6 +5,6 @@ namespace Crossoverse.Core.Domain.Multiplayer
     public interface INetworkObject : IDisposable
     {
         Guid OriginalObjectId { get; }
-        int InstanceId { get; set; }
+        int InstanceId { get; }
     }
 }
--- src/Crossoverse.Core/Domain/Multiplayer/INetworkObjectFactory.cs
+++ src/Crossoverse.Core/Domain/Multiplayer/INetworkObjectFactory.cs
@@ -3,6 +3,7 @@ namespace Crossoverse.Core.Domain.Multiplayer
     public interface INetworkObjectFactory
     {
         INetworkObject Create(IGuidComponent prefab);
+        INetworkObject Create(IGuidComponent prefab, int instanceId);
         int NewInstanceId();
     }
 }
